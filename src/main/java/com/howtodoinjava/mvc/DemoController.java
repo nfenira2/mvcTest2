6e528946719c5caf90fbdecba89b2ada0b58dc51
nfenira <naoufelfennira@gmail.com>
2017-02-19 23:54:12 +0100

    conf2

--- src/main/java/com/howtodoinjava/mvc/DemoController.java
+++ src/main/java/com/howtodoinjava/mvc/DemoController.java
@@ -13,6 +13,6 @@ public class DemoController
 	public String printWelcome(ModelMap model) 
 	{
 		model.addAttribute("message", "Spring 3 MVC Hello World !! Thanks to www.howtodoinjava.com");
-		return "helloWorld4";
+		return "helloWorld42";
 	}
 }
